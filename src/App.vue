57e8e65ab12585bc66c788f06a761f48627c988e
Marianna <mariannadek@gmail.com>
2023-07-07 17:23:41 +0400

    Vie Cli HW2

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <PageHeader />
+    <div class="product-container center">
+      <div v-for="product in products" :key="product.name">
+        <product-details :product="product"></product-details>
+      </div>
+    </div>
+    <PageFooter />
+  </div>
+</template>
+  
+<script>
+import PageHeader from './components/PageHeader.vue';
+import ProductDetails from './components/ProductDetails.vue';
+import PageFooter from './components/PageFooter.vue';
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+    ProductDetails,
+    PageFooter,
+  },
+  data() {
+    return {
+      products: [
+        {
+          name: 'Product 1',
+          price: 17.05,
+          available: true
+        },
+        {
+          name: 'Product 2',
+          price: 2029.99,
+          available: false
+        },
+        {
+          name: 'Product 3',
+          price: 589.00,
+          available: true
+        }
+      ]
+    };
+  }
+};
+</script>
+
+
+<style lang="scss">
+@import "@/styles/style.scss";
+</style>
---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="productContent">
+        <h3>{{ product.name }}</h3>
+        <p>{{ formattedPrice }}</p>
+        <p>{{ availability }}</p>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: {
+        product: {}
+    },
+    computed: {
+        formattedPrice() {
+            const formatted = this.product.price.toFixed(2);
+            return '$' + formatted;
+        },
+        availability() {
+            return this.product.available ? 'Available' : 'Out of stock';
+        }
+    }
+};
+</script>
+  
